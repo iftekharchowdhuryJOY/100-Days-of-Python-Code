7de94b426f89c032d08a0f5a80605d51e23c5100
iftekhar <iftekharislamchowdhury@gmail.com>
2023-11-12 23:48:22 -0500

    initial commit

---
+++ BMI_INDEX.py
@@ -0,0 +1,9 @@
+height = input("Enter your height: ")
+weight = input("Enter your weight: ")
+
+weight_as_int = int(weight)
+height_as_float = float(height);
+
+bmi = weight_as_int / height_as_float ** 2
+bmi_as_int = int(bmi)
+print("Your BMI: ", bmi_as_int)
