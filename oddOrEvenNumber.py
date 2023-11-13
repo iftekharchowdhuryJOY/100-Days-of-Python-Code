9b9850cbeb5672dedf6b99b75317a31a8564ebb6
iftekhar <iftekharislamchowdhury@gmail.com>
2023-11-13 01:40:10 -0500

    even or odd number finding

---
+++ oddOrEvenNumber.py
@@ -0,0 +1,8 @@
+print("Finding the odd or even number")
+number = int(input("enter your number: "))
+
+if number % 2 == 0:
+    print(f"{number} is a even number")
+else:
+    print(f"{number} is a odd number")
+
